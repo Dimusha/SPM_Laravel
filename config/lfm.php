ad8fce2ce02d0f0e4b7e6d79389a225766f7b1a4
Navodya Himashi Jayasinghe <navodya.jayasinghe55@gmail.com>
2021-09-13 19:56:17 +0530

    database files updating

--- config/lfm.php
+++ config/lfm.php
@@ -1,49 +1,22 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Documentation for this config :
-|--------------------------------------------------------------------------
-| online  => http://unisharp.github.io/laravel-filemanager/config
-| offline => vendor/unisharp/laravel-filemanager/docs/config.md
- */
-
 return [
-    /*
-    |--------------------------------------------------------------------------
-    | Routing
-    |--------------------------------------------------------------------------
-     */
+    
+    // Routing
 
     'use_package_routes'       => true,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Shared folder / Private folder
-    |--------------------------------------------------------------------------
-    |
-    | If both options are set to false, then shared folder will be activated.
-    |
-     */
-
+    // Shared folder / Private folder
+  
     'allow_private_folder'     => true,
 
-    // Flexible way to customize client folders accessibility
-    // If you want to customize client folders, publish tag="lfm_handler"
-    // Then you can rewrite userField function in App\Handler\ConfigHandler class
-    // And set 'user_field' to App\Handler\ConfigHandler::class
-    // Ex: The private folder of user will be named as the user id.
     'private_folder_name'      => UniSharp\LaravelFilemanager\Handlers\ConfigHandler::class,
 
     'allow_shared_folder'      => false,
 
     'shared_folder_name'       => 'shares',
 
-    /*
-    |--------------------------------------------------------------------------
-    | Folder Names
-    |--------------------------------------------------------------------------
-     */
+    // Folder Names
 
     'folder_categories'        => [
         'file'  => [
@@ -74,21 +47,13 @@ return [
         ],
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Pagination
-    |--------------------------------------------------------------------------
-     */
+    // Pagination
 
     'paginator' => [
         'perPage' => 30,
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | Upload / Validation
-    |--------------------------------------------------------------------------
-     */
+    // Upload / Validation
 
     'disk'                     => 'public',
 
@@ -102,18 +67,8 @@ return [
 
     'should_validate_mime'     => false,
 
-    // behavior on files with identical name
-    // setting it to true cause old file replace with new one
-    // setting it to false show `error-file-exist` error and stop upload
     'over_write_on_duplicate'  => false,
 
-    /*
-    |--------------------------------------------------------------------------
-    | Thumbnail
-    |--------------------------------------------------------------------------
-     */
-
-    // If true, image thumbnails would be created during upload
     'should_create_thumbnails' => true,
 
     'thumb_folder_name'        => 'thumbs',
@@ -129,11 +84,8 @@ return [
 
     'thumb_img_height'         => 200, // px
 
-    /*
-    |--------------------------------------------------------------------------
-    | File Extension Information
-    |--------------------------------------------------------------------------
-     */
+    // File Extension Information
+
 
     'file_type_array'          => [
         'pdf'  => 'Adobe Acrobat',
@@ -150,17 +102,8 @@ return [
         'pptx' => 'Microsoft PowerPoint',
     ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | php.ini override
-    |--------------------------------------------------------------------------
-    |
-    | These values override your php.ini settings before uploading files
-    | Set these to false to ingnore and apply your php.ini settings
-    |
-    | Please note that the 'upload_max_filesize' & 'post_max_size'
-    | directives are not supported.
-     */
+    // php.ini override
+
     'php_ini_overrides'        => [
         'memory_limit' => '256M',
     ],
