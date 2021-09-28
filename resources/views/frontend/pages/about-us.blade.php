396ff5cd3bfdc7232ec5685ed4e64810b7d7e7c8
Dimusha <dimusha.perera@gmail.com>
2021-09-28 19:57:36 +0530

    added frontend files

---
+++ resources/views/frontend/pages/about-us.blade.php
@@ -0,0 +1,185 @@
+@extends('frontend.layouts.master')
+
+@section('main-content')
+
+	<div class="breadcrumbs">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="bread-inner">
+						<ul class="bread-list">
+							<li><a href="index1.html">Home<i class="ti-arrow-right"></i></a></li>
+							<li class="active"><a href="blog-single.html">About Us</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+	<!-- About Us -->
+	<section class="about-us section">
+			<div class="container">
+				<div class="row">
+					<div class="col-lg-6 col-12">
+						<div class="about-content">
+							@php
+								$settings=DB::table('settings')->get();
+							@endphp
+							<h3>Welcome To <span>69 Ecommerce</span></h3>
+							<p>@foreach($settings as $data) {{$data->description}} @endforeach</p>
+							<div class="button">
+								<a href="{{route('blog')}}" class="btn">Our Blog</a>
+								<a href="{{route('contact')}}" class="btn primary">Contact Us</a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-6 col-12">
+						<div class="about-img overlay">
+							{{-- <div class="button">
+								<a href="https://www.youtube.com/watch?v=nh2aYrGMrIE" class="video video-popup mfp-iframe"><i class="fa fa-play"></i></a>
+							</div> --}}
+							<img src="@foreach($settings as $data) {{$data->photo}} @endforeach" alt="@foreach($settings as $data) {{$data->photo}} @endforeach">
+						</div>
+					</div>
+				</div>
+			</div>
+	</section>
+	<!-- End About Us -->
+	
+	{{-- <section id="team" class="team section">
+		<div class="container">
+			<div class="row">
+				<div class="col-12">
+					<div class="section-title">
+						<h2>Our Expert Team</h2>
+						<p>Business consulting excepteur sint occaecat cupidatat consulting non proident, sunt in culpa qui officia deserunt laborum market. </p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-team">
+						<div class="image">
+							<img src="images/team/team1.jpg" alt="#">
+						</div>
+						<div class="info-head">
+							<div class="info-box">
+								<h4 class="name"><a href="#">Dahlia Moore</a></h4>
+								<span class="designation">Senior Manager</span>
+							</div>
+							<div class="social-links">
+								<ul class="social">
+									<li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-behance"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>	
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-team">
+						<div class="image">
+							<img src="images/team/team2.jpg" alt="#">
+						</div>
+						<div class="info-head">
+							<div class="info-box">
+								<h4 class="name"><a href="#">Jhone digo</a></h4>
+								<span class="designation">Markeitng</span>
+							</div>
+							<div class="social-links">
+								<ul class="social">
+									<li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-behance"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>	
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-team">
+						<div class="image">
+							<img src="images/team/team3.jpg" alt="#">
+						</div>
+						<div class="info-head">
+							<div class="info-box">
+								<h4 class="name"><a href="#">Zara tingo</a></h4>
+								<span class="designation">Web Developer</span>
+							</div>
+							<div class="social-links">
+								<ul class="social">
+									<li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-behance"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>	
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-team">
+						<div class="image">
+							<img src="images/team/team4.jpg" alt="#">
+						</div>
+						<div class="info-head">
+							<div class="info-box">
+								<h4 class="name"><a href="#">David Zone</a></h4>
+								<span class="designation">SEO Expert</span>
+							</div>
+							<div class="social-links">
+								<ul class="social">
+									<li><a href="#"><i class="fa fa-facebook-f"></i></a></li>
+									<li><a href="#"><i class="fa fa-twitter"></i></a></li>
+									<li><a href="#"><i class="fa fa-behance"></i></a></li>
+									<li><a href="#"><i class="fa fa-instagram"></i></a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>	
+			</div>	
+		</div>
+	</section> --}}
+
+	<section class="shop-services section">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-service">
+						<i class="ti-rocket"></i>
+						<h4>Free delivery</h4>
+						<p>Within 5km</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-service">
+						<i class="ti-reload"></i>
+						<h4>Free Return</h4>
+						<p>Within 30 days returns</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-service">
+						<i class="ti-lock"></i>
+						<h4>Sucure Payment</h4>
+						<p>100% secure payment</p>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 col-12">
+					<div class="single-service">
+						<i class="ti-tag"></i>
+						<h4>Best Peice</h4>
+						<p>Guaranteed price</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	
+	@include('frontend.layouts.newsletter')
+@endsection
